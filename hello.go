63cbd9444c867440d0729b768d3b4d95a49ed42c
Leah Roberson <77908412+leah-crypto@users.noreply.github.com>
2023-02-03 21:56:20 -0600

    updateee

--- hello.go
+++ hello.go
@@ -10,9 +10,12 @@ func main()  {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Leah")
+
+names := []string{"Bob", "Bill", "Jane"}
+
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(message)
+	fmt.Println(messages)
 }
